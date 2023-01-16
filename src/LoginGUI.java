62adfa653edff4a9650769524faa7d61d7ea2b15
jorgefernandezr <jorge.fernandezr@edu.uah.es>
2023-01-16 19:10:46 +0100

    Borrar proyecto

--- src/LoginGUI.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class LoginGUI {
-    private JPanel panel1;
-    private JLabel IntroLabel;
-    private JTextField textField1;
-    private JPasswordField passwordField1;
-}
