b95d74c46b27c2a5c5cf324db0c257f0183d7b9d
icaceres <icaceresg2001@gmail.com>
2023-01-16 18:48:02 +0100

    First changes

---
+++ src/LoginGUI.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class LoginGUI {
+    private JPanel panel1;
+    private JLabel IntroLabel;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+}
