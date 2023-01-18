454c4c008bcf334ab0550b35a322c2f3e81bc5bf
icaceres <icaceresg2001@gmail.com>
2023-01-18 22:41:03 +0100

    Singleton and Adapter patterns added

--- PlayStorm/src/main/java/Class/Administrator.java
+++ PlayStorm/src/main/java/Class/Administrator.java
@@ -1,8 +1,22 @@
 package Class;
 
-public class Administrator extends User {
 
-    public Administrator(String name, String email, String password) {
+//Singleton pattern
+public final class Administrator extends User{
+    private static Administrator instance;
+    
+    
+    private Administrator(String name, String password, String email)
+    {
         super(name, email, password);
+    }
+    
+    
+    //Call this function to create Administrator user
+    public static Administrator getInstance(String name, String password, String email)
+    {
+        if (instance == null)
+            instance = new Administrator(name, password, email);
+        return instance;
     }
 }
---
+++ PlayStorm/src/main/java/Class/PasswordSHAAdapter.java
@@ -0,0 +1,24 @@
+
+package Class;
+
+public class PasswordSHAAdapter extends Password{
+    
+    private PasswordSHA passwordSHA;
+    
+    public PasswordSHAAdapter()
+    {
+        super();
+    }
+    
+    @Override
+    public Password getPassword()
+    {
+        //return this.name;
+    }
+    
+    @Override
+    public void setPassword()
+    {
+        
+    }
+}
