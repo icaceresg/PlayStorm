14ca4cddc2370909e2d806d4401da423b31f9413
icaceres <icaceresg2001@gmail.com>
2023-01-20 18:55:24 +0100

    id added to product

--- PlayStorm/src/main/java/Class/Product.java
+++ PlayStorm/src/main/java/Class/Product.java
@@ -1,17 +1,32 @@
 package Class;
 
+import Class.Iterator.ProductIterator;
 import java.io.Serializable;
 
 public class Product implements Serializable{
     private String name, description;
     private float price;
     private int amount;
+    private int id;
 
-    public Product(String name, String description, float price, int amount) {
+    public Product(String name, String description, float price, int amount) throws Exception {
         this.name = name;
         this.description = description;
         this.price = price;
         this.amount = amount;
+        ProductIterator iterator = new ProductIterator();
+        if (!iterator.hasNext())
+            this.id = 0;
+        else
+            this.id = new ProductIterator().lastItem().getId() + 1;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
