bf9e169a54420f9022347bf900e7b7bd5386fd34
icaceres <icaceresg2001@gmail.com>
2023-01-21 14:39:04 +0100

    title g and s added

--- PlayStorm/src/main/java/Class/Product.java
+++ PlayStorm/src/main/java/Class/Product.java
@@ -32,12 +32,12 @@ public class Product implements Serializable {
         this.id = id;
     }
 
-    public String getName() {
+    public String getTitle() {
         return title;
     }
 
-    public void setName(String name) {
-        this.title = name;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getDescription() {
